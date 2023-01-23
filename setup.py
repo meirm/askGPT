3b74735266a8027c79d4366102823f05245cedd9
Meir Michanie <meirm@cyborg.fi>
2023-01-23 09:53:20 +0100

    move code to src and sample data to config dir

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
     name='genie',
+    package_dir={'':'src'},
     version='0.2.0',
     py_modules=['genie'],
     install_requires=[
         'Click',
         'rich',
         'openai'
+    ],
+    include_package_data=True,
+    include_dirs=[
+        'config'
     ],
     entry_points={
         'console_scripts': [
