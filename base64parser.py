f3d2ec36f32dc014233badf8e108caa8a7f5c441
Meir Michanie <meirm@riunx.com>
2023-12-24 21:08:05 +0100

    aider: Added functionality to encode and decode text to/from base64 with argument parsing.
    # Aider chat conversation:
    
    USER: read from stdin and write to stdout translating from clear text to base64 of called with argument -e/--encode or from base64 to cleartext if called with the argument -d/--decode
    
    ASSISTANT: To implement this functionality, we need to modify `base64parser.py`:
    
    1. Import the required modules.
    2. Define a function to encode clear text to base64.
    3. Define a function to decode base64 to clear text.
    4. Add argument parsing to handle -e/--encode and -d/--decode options.
    5. Add a main function to read from stdin, call the appropriate function based on the argument passed, and write the result to stdout.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    base64parser.py
    ```python
    <<<<<<< SEARCH
    =======
    import argparse
    import base64
    import sys
    
    def encode_to_base64(clear_text):
        return base64.b64encode(clear_text.encode()).decode()
    
    def decode_from_base64(base64_text):
        return base64.b64decode(base64_text.encode()).decode()
    
    def main():
        parser = argparse.ArgumentParser()
        group = parser.add_mutually_exclusive_group(required=True)
        group.add_argument('-e', '--encode', action='store_true')
        group.add_argument('-d', '--decode', action='store_true')
        args = parser.parse_args()
    
        input_text = sys.stdin.read().strip()
    
        if args.encode:
            result = encode_to_base64(input_text)
        elif args.decode:
            result = decode_from_base64(input_text)
    
        sys.stdout.write(result)
    
    if __name__ == '__main__':
        main()
    >>>>>>> REPLACE
    ```
    This code will allow you to use the script as follows:
    
    - To encode clear text to base64: `echo "clear text" | python base64parser.py -e`
    - To decode base64 to clear text: `echo "Y2xlYXIgdGV4dA==" | python base64parser.py -d`

---
+++ base64parser.py
@@ -0,0 +1,28 @@
+import argparse
+import base64
+import sys
+
+def encode_to_base64(clear_text):
+    return base64.b64encode(clear_text.encode()).decode()
+
+def decode_from_base64(base64_text):
+    return base64.b64decode(base64_text.encode()).decode()
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-e', '--encode', action='store_true')
+    group.add_argument('-d', '--decode', action='store_true')
+    args = parser.parse_args()
+
+    input_text = sys.stdin.read().strip()
+
+    if args.encode:
+        result = encode_to_base64(input_text)
+    elif args.decode:
+        result = decode_from_base64(input_text)
+
+    sys.stdout.write(result)
+
+if __name__ == '__main__':
+    main()
